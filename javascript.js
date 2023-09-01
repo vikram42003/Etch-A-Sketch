09d3ccf1a5fadf34900eefb009d636d7d7035847
Vikram <vikramjit360@gmail.com>
2023-09-01 21:51:01 +0530

    Add drawGrid function, Update grid-cotainer and grid-box html and css

--- javascript.js
+++ javascript.js
@@ -2,15 +2,23 @@
 
 const initial_gridSize = 16;
 
-drawGrid(initial_gridSize);
+drawGrid(initial_gridSize / 2, initial_gridSize);
 
 
 
 
 
-function drawGrid(gridSize) {
-  const gridContainer = document.getElementByClass("grid-container");
-  const gridFragment = new documentFragmant();
+function drawGrid(gridSize_Height, gridSize_Width) {
+  const gridContainer = document.querySelector(".grid-container");
+  const gridFragment = new DocumentFragment();
 
-  
+  const gridSize_Area = gridSize_Height * gridSize_Width;
+  for (let i = 0; i < gridSize_Area; i++) {
+    let box = document.createElement("div");
+    box.classList.add("grid-box");
+    gridFragment.appendChild(box);
+  }
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize_Width}, auto)`;
+  gridContainer.appendChild(gridFragment);
 }
