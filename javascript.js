4738d3dd655ecf979e3ae2fe65200e483441fef7
Vikram <vikramjit360@gmail.com>
2023-09-01 15:52:36 +0530

    Rename 'sketch-container' to 'grid-container', Update css to make it flexible

---
+++ javascript.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const initial_gridSize = 16;
+
+drawGrid(initial_gridSize);
+
+
+
+
+
+function drawGrid(gridSize) {
+  const gridContainer = document.getElementByClass("grid-container");
+  const gridFragment = new documentFragmant();
+
+  
+}
