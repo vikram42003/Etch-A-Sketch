62f32abb700d979f7f64bbe9e76a94a302a6b641
Vikram <vikramjit360@gmail.com>
2023-09-04 22:13:45 +0530

    add eraser and reset board functions, fir minor error in change grid box color functions

--- javascript.js
+++ javascript.js
@@ -10,10 +10,22 @@ addButtonEventListeners();
 // ADD GRADIENT ANIMATIONS BUTTON FOR FUN !!!
 
 
+
+
+/*  NOTE - Right now the functions like toggle grid, reset board make changes 
+    by iteration over each box one by one. Which is quite inefficient imo
+    when you come back to this project then work on improving overall efficiency
+    and maybe add additional functionality
+*/
+
 function addButtonEventListeners() {
   const toggleGrid_Button = document.getElementById("toggle-grid");
+  const eraser_Button = document.getElementById("eraser-button");
+  const resetBoard_Button = document.getElementById("reset-board");
 
   toggleGrid_Button.addEventListener("click", toggleGrid);
+  eraser_Button.addEventListener("click", eraser);
+  resetBoard_Button.addEventListener("click", resetBoard);
 }
 
 function drawGrid(gridSize_Height, gridSize_Width) {
@@ -43,8 +55,7 @@ function drawGrid(gridSize_Height, gridSize_Width) {
 
 // Changes the color of the initial box
 function changeGridBoxColor(event) {
-    if (!event.target.style.backgroundColor &&
-      event.target.classList.contains("grid-box")) {
+    if (event.target.classList.contains("grid-box")) {
       event.target.style.backgroundColor = color;
     }
 }
@@ -55,14 +66,25 @@ function changeSubsequentGridBoxColor(event) {
   // AND left mouse button is being clicked
   // AND event is not triggered by a grid box not the container itself
   // then change it to currently selected color
-  if (!event.target.style.backgroundColor &&
-      event.buttons === 1 &&
+  if (event.buttons === 1 &&
       event.target.classList.contains("grid-box")) {
     event.target.style.backgroundColor = color;
   }
 }
 
 
+
+function eraser() {
+  // Changes the box color on click to be pure white
+  color = "#FFFFFF";
+}
+
+function resetBoard() {
+  const boxes = document.querySelectorAll(".grid-box");
+  boxes.forEach(box => {
+    box.style.backgroundColor = "#FFFFFF";
+  })
+}
 
 function toggleGrid(event) {
   event.target.classList.toggle("toggle-button-active");
